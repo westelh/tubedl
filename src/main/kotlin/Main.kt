32f182b47bc6b942e88cd888b83e37faf47cb0d7
westelh <westelh@users.noreply.github.com>
2017-10-29 18:21:43 +0900

    Add OAuth2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import javafx.application.Application
 import javafx.event.EventHandler
 import javafx.scene.Scene
+import javafx.scene.control.Alert
 import javafx.scene.control.Button
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
@@ -13,6 +14,9 @@ fun main(args: Array<String>) {
 }
 
 class Main: Application() {
+    lateinit var auth: OAuthPageView
+    lateinit var stage: Stage
+
     companion object {
         fun main(args: Array<String>) {
         }
@@ -20,18 +24,36 @@ class Main: Application() {
 
     override fun start(primaryStage: Stage?) {
         if (primaryStage == null) throw NullPointerException("stage is not available")
-        else {
-            primaryStage.apply {
-                val parent = GridPane().apply {
-                    children.add(Button().apply {
-                        text = "Google OAuth2認証"
-                        onAction = EventHandler {
-                            OAuthPageView().launch()
-                        }
-                    })
-                }
-                scene = Scene(parent, 300.0, 250.0, false)
-            }.show()
-        }
+        stage = primaryStage
+        auth = OAuthPageView(this)
+        stage.apply {
+            val parent = GridPane().apply {
+                add(Button().apply {
+                    text = "Google OAuth2認証"
+                    onAction = EventHandler {
+                        auth.launch()
+                    }
+                }, 0, 1)
+                add(Button().apply {
+                    text = "print token"
+                    onAction = EventHandler {
+                        println(auth.token?:"have not received")
+                    }
+                }, 0, 2)
+                add(Button().apply {
+                    text = "認証状態の確認"
+                    onAction = EventHandler {
+                        notifyAuthState()
+                    }
+                }, 0, 3)
+            }
+            scene = Scene(parent, 300.0, 250.0, false)
+        }.show()
+    }
+
+    fun notifyAuthState() {
+        Alert(Alert.AlertType.INFORMATION).apply {
+            contentText = "Google OAuth2認証は" + if (auth.token==null) "完了していません" else "確認済みです"
+        }.show()
     }
 }
--- src/main/kotlin/OAuthPageView.kt
+++ src/main/kotlin/OAuthPageView.kt
@@ -1,20 +1,74 @@
+import javafx.application.Application
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
+import javafx.concurrent.Worker
 import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.web.WebView
+import javafx.stage.Modality
 import javafx.stage.Stage
+import javafx.stage.Window
+import org.apache.oltu.oauth2.client.OAuthClient
+import org.apache.oltu.oauth2.client.URLConnectionClient
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest
+import org.apache.oltu.oauth2.common.OAuthProviderType
+import org.apache.oltu.oauth2.common.message.types.GrantType
 
-class OAuthPageView {
-    val stage = Stage().apply {
+class OAuthPageView(val owner: Main): ChangeListener<Worker.State> {
+    private val clientID = "697386451833-vmi5vcs5e6kh5ufb8eopg6ea7eo8gma9.apps.googleusercontent.com"
+    private val scope = "https://www.googleapis.com/auth/youtube"
+    var token: String? = null
+
+    private val webview = WebView().apply {
+        engine.load(genRequest().locationUri)
+        engine.loadWorker.stateProperty().addListener(this@OAuthPageView)
+    }
+
+    private val stage = Stage().apply {
         title = "TubeDL"
         val group = Group().apply {
-            children.add(WebView().apply {
-                engine.load("https://accounts.google.com/o/oauth2/auth")
-            })
+            children.add(webview)
         }
-        scene = Scene(group, 300.0, 250.0, false)
+        initModality(Modality.APPLICATION_MODAL)
+        initOwner(this@OAuthPageView.owner.stage)
+        scene = Scene(group, 800.0, 600.0, false)
+    }
+
+    private fun genRequest(): OAuthClientRequest {
+        return OAuthClientRequest.authorizationProvider(OAuthProviderType.GOOGLE).apply {
+            setClientId(clientID)
+            setRedirectURI("http://localhost")
+            setScope(scope)
+            setResponseType("code")
+        }.buildQueryMessage()
     }
 
     fun launch() {
         stage.show()
+    }
+
+    override fun changed(observable: ObservableValue<out Worker.State>?, oldValue: Worker.State?, newValue: Worker.State?) {
+        println(newValue)
+        if (newValue == Worker.State.FAILED) {
+            println("location is " + webview.engine.location)
+            val code = webview.engine.location.split("code=")[1]
+            println("extracted code is " + code)
+
+            val tokenReq = OAuthClientRequest.tokenProvider(OAuthProviderType.GOOGLE).apply {
+                setClientId(clientID)
+                setGrantType(GrantType.AUTHORIZATION_CODE)
+                setCode(code)
+                setClientSecret("vHGsqXiEgzc9lPCtahbTdfbY")
+                setRedirectURI("http://localhost")
+            }.buildBodyMessage()
+
+            println(tokenReq.body)
+
+            val res = OAuthClient(URLConnectionClient()).accessToken(tokenReq, "POST")
+            println("token=" + res.accessToken)
+            token = res.accessToken
+            stage.close()
+            this@OAuthPageView.owner.notifyAuthState()
+        }
     }
 }
