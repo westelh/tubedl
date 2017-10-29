64c81077d79d5bc1a62c8963c6c0f1f9ae828d5f
westelh <westelh@users.noreply.github.com>
2017-10-29 12:40:05 +0900

    Switch main call to JavaFX

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
+import javafx.application.Application
+import javafx.stage.Stage
+
 fun main(args: Array<String>) {
     val req = YoutubeAccess().youtube.channels().list("contentDetails").setMine(true)
     req.fields = "items/contentDetails,nextPageToken,pageInfo"
     val res = req.execute()
     print(res.items)
+}
+
+class Main: Application() {
+    companion object {
+        fun main(args: Array<String>) {
+        }
+    }
+
+    override fun start(primaryStage: Stage?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
